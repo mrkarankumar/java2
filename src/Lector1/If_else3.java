34b64bb1c1534cf48347cf765d94829e64e11874
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-08-07 19:57:56 +0530

    If_else3

---
+++ src/Lector1/If_else3.java
@@ -0,0 +1,21 @@
+package Lector1;
+
+public class If_else3 {
+
+    public static void main(String[] args) {
+
+        boolean karan_young_student =true;
+        boolean amin_not_young_student = false;
+        if (karan_young_student){
+            System.out.println("hello karan kumar");
+        } else if (amin_not_young_student){
+            System.out.println("hello amin you are not young boy");
+
+        } else {
+
+            System.out.println("my name is karan kumar");
+
+        }
+
+    }
+}
